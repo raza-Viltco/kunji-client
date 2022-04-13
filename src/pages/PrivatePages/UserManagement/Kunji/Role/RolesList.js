45cacb91b6fef8edd07902292091daca054ff0b4
Saad Mehmood <saad.mehmood@viltco.com>
2022-04-13 11:18:43 +0500

    Role List Card

--- src/pages/PrivatePages/UserManagement/Kunji/Role/RolesList.js
+++ src/pages/PrivatePages/UserManagement/Kunji/Role/RolesList.js
@@ -5,57 +5,78 @@ import "./RoleList.css";
 
 const RoleList = () => {
   return (
-    <>
+    <div className="col-sm-12">
       <h3>Defined Roles</h3>
-      <div className="card-background col-md-4">
-        <div className=" define-roles-inner-flex manager-block-style">
-          <div className="col-md-3">
-            <p>Manager</p>
+      <div className="card-background col-md-4 ">
+        <div className="row background-color-header">
+          <div className="col-sm-6 m-auto pt-2">
+            <h5>
+              <b>Manager</b>
+            </h5>
           </div>
-          <div className="col-md-3 roles-permission-inner">
-            <p>Select All</p>
-            <Switch />
-          </div>
-        </div>
-
-        <div className=" define-roles-inner-flex">
-          <div className="col-md-3">
-            <p>User Creation</p>
-          </div>
-          <div className="col-md-3 roles-permission-inner">
-            <Switch />
-          </div>
-        </div>
-
-        <div className=" define-roles-inner-flex">
-          <div className="col-md-3">
-            <p>Approval</p>
-          </div>
-          <div className="col-md-3 roles-permission-inner">
-            <Switch />
+          <div className="col-sm-6">
+            <div className="row">
+              <div className="col-sm-6 m-auto pt-2">
+                <h6>
+                  <b>Select All</b>
+                </h6>
+              </div>
+              <div className="col-sm-6   text-end">
+                <Switch />
+              </div>
+            </div>
           </div>
         </div>
-
-        <div className=" define-roles-inner-flex">
-          <div className="col-md-3">
-            <p>View Listing</p>
+        <div className="row background-color-header1">
+          <div className="col-sm-6">
+            <h5 className="pt-2">
+              <b>User Creation</b>
+            </h5>
           </div>
-          <div className="col-md-3 roles-permission-inner">
-            <Switch />
-          </div>
-        </div>
-
-        <div className=" define-roles-inner-flex">
-          <div className="col-md-3">
-            <p>Soft Delete</p>
-          </div>
-          <div className="col-md-3 roles-permission-inner">
-            <Switch />
+          <div className="col-sm-6 ">
+            <div className="col-sm-12 text-end">
+              <Switch />
+            </div>
           </div>
         </div>
-
+        <div className="row background-color-header1">
+          <div className="col-sm-6">
+            <h5 className="pt-2">
+              <b>Approval</b>
+            </h5>
+          </div>
+          <div className="col-sm-6 ">
+            <div className="col-sm-12 text-end">
+              <Switch />
+            </div>
+          </div>
+        </div>
+        <div className="row background-color-header1">
+          <div className="col-sm-6">
+            <h5 className="pt-2">
+              <b>View Listing</b>
+            </h5>
+          </div>
+          <div className="col-sm-6 ">
+            <div className="col-sm-12 text-end">
+              <Switch />
+            </div>
+          </div>
+        </div>
+        <div className="row background-color-header1">
+          <div className="col-sm-6">
+            <h5 className="pt-2">
+              <b>Soft Delete</b>
+            </h5>
+          </div>
+          <div className="col-sm-6 ">
+            <div className="col-sm-12 text-end">
+              <Switch />
+            </div>
+          </div>
+        </div>
       </div>
-    </>
+    </div>
   );
 };
 export default RoleList;
