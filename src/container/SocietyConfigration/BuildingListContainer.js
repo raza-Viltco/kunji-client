3612b95e88370548446af92337b7cbcd6b768a4b
Ali Raza <59689182+alincbian@users.noreply.github.com>
2022-06-29 17:07:58 +0500

    Property Management Permissions

--- src/container/SocietyConfigration/BuildingListContainer.js
+++ src/container/SocietyConfigration/BuildingListContainer.js
@@ -1,26 +1,31 @@
 import { React, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 
-import { appartmentList,buildingData } from "../../redux/actions/SocietyConfiguration/SocietyConfiguration";
+import {
+  appartmentList,
+  buildingData,
+} from "../../redux/actions/SocietyConfiguration/SocietyConfiguration";
 
 const buildingListContainer = (BuildingList) => () => {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    dispatch(appartmentList());
-  }, []);
-
-  useEffect(() => {
     dispatch(buildingData());
   }, []);
 
-  const buildingArrData = useSelector((state) => state.societyConfig.verticalData);
+  const buildingArrData = useSelector(
+    (state) => state.societyConfig.verticalData
+  );
   const stateLoading = useSelector((state) => state.local.isLoading);
 
-  console.log(buildingArrData)
+  console.log(buildingArrData);
 
-  return <BuildingList
-   buildingArrData={buildingArrData} stateLoading={stateLoading} />;
+  return (
+    <BuildingList
+      buildingArrData={buildingArrData}
+      stateLoading={stateLoading}
+    />
+  );
 };
 
 export default buildingListContainer;
--- src/pages/PrivatePages/SocietyConfigration/HorizontalSociety/addPlots.js
+++ src/pages/PrivatePages/SocietyConfigration/HorizontalSociety/addPlots.js
@@ -10,6 +10,7 @@ import InputError from "../../../../components/Form/InputError";
 import plotContainer from "../../../../container/SocietyConfigration/PlotContainer";
 import { setMappingId } from "../../../../redux/actions/SocietyConfiguration/SocietyConfiguration";
 import AddPlotListing from "./addPlotList";
+import { checkForPermission } from "../../../../utils/functions";
 import "./addPlots.css";
 
 const AddPlots = ({
@@ -24,107 +25,109 @@ const AddPlots = ({
 
   return (
     <Card>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={areaMappingHandler}
-      >
-        {(props) => (
-          <Form>
-            {console.log(props)}
-            <h3>Add Plots</h3>
-            <div className="row">
-              <div className="col-md-6">
-                <Dropdown
-                  inputLabel="Select Sector/Block No"
-                  name="building"
-                  value={props.values.building}
-                  onChange={(e) => {
-                    props.handleChange(e);
-                    dispatch(setMappingId(e.target.value));
-                  }}
-                  className={
-                    props?.errors?.building && props?.touched?.building
-                      ? "input-outline"
-                      : "bootstyle"
-                  }
-                >
-                  <option>Select Sector/Block No</option>
-                  {buildingArr.map((item, index) => {
-                    return (
-                      <option value={item.id} key={index}>
-                        {item.sector_block_building}
-                      </option>
-                    );
-                  })}
-                </Dropdown>
-                {props?.touched?.building && props?.errors?.building && (
-                  <InputError>{props?.errors?.building}</InputError>
-                )}
+      {checkForPermission("Property Mgmt.", "level2") && (
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={areaMappingHandler}
+        >
+          {(props) => (
+            <Form>
+              {console.log(props)}
+              <h3>Add Plots</h3>
+              <div className="row">
+                <div className="col-md-6">
+                  <Dropdown
+                    inputLabel="Select Sector/Block No"
+                    name="building"
+                    value={props.values.building}
+                    onChange={(e) => {
+                      props.handleChange(e);
+                      dispatch(setMappingId(e.target.value));
+                    }}
+                    className={
+                      props?.errors?.building && props?.touched?.building
+                        ? "input-outline"
+                        : "bootstyle"
+                    }
+                  >
+                    <option>Select Sector/Block No</option>
+                    {buildingArr.map((item, index) => {
+                      return (
+                        <option value={item.id} key={index}>
+                          {item.sector_block_building}
+                        </option>
+                      );
+                    })}
+                  </Dropdown>
+                  {props?.touched?.building && props?.errors?.building && (
+                    <InputError>{props?.errors?.building}</InputError>
+                  )}
+                </div>
+                <div className="col-md-6">
+                  <Dropdown
+                    inputLabel="Street"
+                    name="floor"
+                    value={props.values.floor}
+                    onChange={props.handleChange}
+                    className={
+                      props?.errors?.floor && props?.touched?.floor
+                        ? "input-outline"
+                        : "bootstyle"
+                    }
+                  >
+                    <option>Select Street</option>
+                    {floorArr.map((item, index) => {
+                      return (
+                        <option value={item.id} key={index}>
+                          {item.street_floor}
+                        </option>
+                      );
+                    })}
+                  </Dropdown>
+                  {props?.touched?.floor && props?.errors?.floor && (
+                    <InputError>{props?.errors?.floor}</InputError>
+                  )}
+                </div>
+                <div className="col-md-6 mt-3">
+                  <Input
+                    placeholder="Plot/House No"
+                    name="plot"
+                    value={props.values.plot}
+                    onChange={props.handleChange}
+                    className={
+                      props?.errors?.plot && props?.touched?.plot
+                        ? "input-outline"
+                        : "bootstyle"
+                    }
+                  />
+                  {props?.touched?.plot && props?.errors?.plot && (
+                    <InputError>{props?.errors?.plot}</InputError>
+                  )}
+                </div>
               </div>
-              <div className="col-md-6">
-                <Dropdown
-                  inputLabel="Street"
-                  name="floor"
-                  value={props.values.floor}
-                  onChange={props.handleChange}
-                  className={
-                    props?.errors?.floor && props?.touched?.floor
-                      ? "input-outline"
-                      : "bootstyle"
-                  }
-                >
-                  <option>Select Street</option>
-                  {floorArr.map((item, index) => {
-                    return (
-                      <option value={item.id} key={index}>
-                        {item.street_floor}
-                      </option>
-                    );
-                  })}
-                </Dropdown>
-                {props?.touched?.floor && props?.errors?.floor && (
-                  <InputError>{props?.errors?.floor}</InputError>
-                )}
-              </div>
-              <div className="col-md-6 mt-3">
-                <Input
-                  placeholder="Plot/House No"
-                  name="plot"
-                  value={props.values.plot}
-                  onChange={props.handleChange}
-                  className={
-                    props?.errors?.plot && props?.touched?.plot
-                      ? "input-outline"
-                      : "bootstyle"
-                  }
-                />
-                {props?.touched?.plot && props?.errors?.plot && (
-                  <InputError>{props?.errors?.plot}</InputError>
-                )}
-              </div>
-            </div>
 
-            <div className="row">
-              <div className="col-sm-9"></div>
-              <div className="col-sm-3">
-                <Button
-                  type="submit"
-                  fullWidth
-                  variant="contained"
-                  sx={{ mt: 3, mb: 2, borderRadius: 20 }}
-                  isLoading={stateLoading}
-                  size="small"
-                >
-                  Add Plots
-                </Button>
+              <div className="row">
+                <div className="col-sm-9"></div>
+                <div className="col-sm-3">
+                  <Button
+                    type="submit"
+                    fullWidth
+                    variant="contained"
+                    sx={{ mt: 3, mb: 2, borderRadius: 20 }}
+                    isLoading={stateLoading}
+                    size="small"
+                  >
+                    Add Plots
+                  </Button>
+                </div>
               </div>
-            </div>
-          </Form>
-        )}
-      </Formik>
+            </Form>
+          )}
+        </Formik>
+      )}
 
-      <AddPlotListing />
+      {checkForPermission("Property Mgmt.", "Full Map") && <AddPlotListing />}
     </Card>
   );
 };
